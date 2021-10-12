834580eaf3072546bd644973c09d5448ddca2352
Aly <aly.elkabani@gmail.com>
2021-10-12 09:21:02 +0200

    neka drevesa i snezinke

--- fraktali/fraktali/Form1.Designer.cs
+++ fraktali/fraktali/Form1.Designer.cs
@@ -35,7 +35,7 @@ namespace fraktali
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1113, 575);
+            this.ClientSize = new System.Drawing.Size(584, 561);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
--- fraktali/fraktali/Form1.cs
+++ fraktali/fraktali/Form1.cs
@@ -26,7 +26,7 @@ namespace fraktali
 
         private int risaniy(double yr)
         {
-            return (int)Math.Round(this.Height-this.Height * yr);
+            return (int)Math.Round(this.Height - this.Height * yr);
         }
         public void obratLevo(double kot)
         {
@@ -36,22 +36,85 @@ namespace fraktali
         {
             double stariX = x;
             double stariY = y;
-            x = x + Math.Cos(alfa * Math.PI / 180);
-            y = y + Math.Sin(alfa * Math.PI / 180);
+            x = x + d * Math.Cos(alfa * Math.PI / 180);
+            y = y + d * Math.Sin(alfa * Math.PI / 180);
             int x1 = risaniX(stariX);
             int y1 = risaniy(stariY);
             int x2 = risaniX(x);
             int y2 = risaniy(y);
-            g.DrawLine(new Pen(Color.Black, width:3), x1, y1, x2, y2);
+            g.DrawLine(new Pen(Color.Black, width: 3), x1, y1, x2, y2);
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
+            /*x = 0.5;
+            y = 0.1;
+            alfa = 0;
+            for(int k=0; k<6; k++)
+            {
+                double korak = 0.2;
+                premik(korak, g);
+                obratLevo(360 / 6);
+            }*/
+
+            /*Random r = new Random();
             x = 0.5;
             y = 0.5;
             alfa = 0;
-            premik(0.5, g);
+            for (int k = 0; k < 10000; k++)
+            {
+                obratLevo(r.Next(360));
+                premik(0.1, g);
+            }*/
+
+            /*x = 0.4;
+            y=0.1;
+            alfa = 0;
+            int n = 9;
+            for (int k=0;k<6;k++)
+            {
+                koch(n, 1 / Math.Pow(3, n)/3, g);
+                obratLevo(360 / 6);
+            }*/
+
+            int n = 10;
+            drevo(n, 0.5, 0, 90, 0.3, g);
+        }
+        public void koch(int n, double korak, Graphics g)
+        {
+            if (n == 0)
+            {
+                premik(korak, g);
+                return;
+            }
+            koch(n - 1, korak, g);
+            obratLevo(60);
+            koch(n-1, korak, g);
+            obratLevo(-120);
+            koch(n - 1, korak, g);
+            obratLevo(60);
+            koch(n - 1, korak, g);
+        }
+        public void drevo(int n, double x, double y, double a, double dolzina, Graphics g)
+        {
+            double cx = x + dolzina * Math.Cos(a * Math.PI / 180);
+            double cy = y + dolzina * Math.Sin(a * Math.PI / 180);
+            int x1 = risaniX(cx);
+            int y1 = risaniy(cy);
+            int x2 = risaniX(x);
+            int y2 = risaniy(y);
+            Random r = new Random();
+            Color c = Color.FromArgb(r.Next(255), r.Next(255), r.Next(255));
+            Pen p = new Pen(c,(float)(Math.Pow(n,1.2)));
+            g.DrawLine(p, x1, y1, x2, y2);
+            if (n == 0)
+            {
+                return;  
+            }
+            drevo(n - 1, cx, cy, a - 69, dolzina * 0.65, g);
+            drevo(n - 1, cx, cy, a + 69, dolzina * 0.65, g);
+            drevo(n - 1, cx, cy, a, dolzina * (1-0.65), g);
         }
     }
 }

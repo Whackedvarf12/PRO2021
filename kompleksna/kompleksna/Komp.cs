19b5f1afbf469c16bd2fa46d80c934c9e73ab493
Aly <aly.elkabani@gmail.com>
2021-10-22 13:23:43 +0200

    opa

---
+++ kompleksna/kompleksna/Komp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kompleksna
+{
+    class Komp
+    {
+        public double Re { get; set; }
+        public double Im { get; set; }
+        public override string ToString()
+        {
+            return Re + " + i*" + Im;
+        }
+        public static Komp operator +(Komp a, Komp b){
+            Komp c = new Komp();
+            c.Re = a.Re+ b.Re;
+            c.Im = a.Im + b.Im;
+            return c;
+        }
+        public static Komp operator -(Komp a, Komp b)
+        {
+            Komp c = new Komp();
+            c.Re = a.Re - b.Re;
+            c.Im = a.Im - b.Im;
+            return c;
+        }
+        public static Komp operator *(Komp a, Komp b)
+        {
+            Komp c = new Komp();
+            c.Re = a.Re * b.Re - a.Im * b.Im;
+            c.Im = a.Re * b.Im + a.Im*b.Re;
+            return c;
+        }
+    }
+}
---
+++ pravokotnik/pravokotnik/Pravokotnik.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pravokotnik
+{
+    class Pravokotnik
+    {
+        private double sirina;
+        private double visina;
+        public Pravokotnik()
+        {
+            this.sirina = 0;
+            this.visina = 0;
+        }
+        public Pravokotnik(double s, double v)
+        {
+            this.sirina = s;
+            this.visina = v;
+        }
+
+        public double Sirina { get => sirina; set => sirina = value; }
+        public double Visina { get => visina; set => visina = value; }
+        public double Ploscina { get => sirina*visina; }
+
+        public static bool operator ==(Pravokotnik b, Pravokotnik c)
+        {
+                return (b.Ploscina == c.Ploscina);
+        }
+        public static bool operator !=(Pravokotnik b, Pravokotnik c)
+        {
+                return (b.Ploscina != c.Ploscina);
+        }
+        public static bool operator >(Pravokotnik b, Pravokotnik c)
+        {
+            return (b.Ploscina > c.Ploscina);
+        }
+        public static bool operator <(Pravokotnik b, Pravokotnik c)
+        {
+            return (b.Ploscina < c.Ploscina);
+        }
+        public static bool operator >=(Pravokotnik b, Pravokotnik c)
+        {
+            return (b.Ploscina >= c.Ploscina);
+        }
+            public static bool operator <=(Pravokotnik b, Pravokotnik c)
+        {
+            return (b.Ploscina <= c.Ploscina);
+        }
+        public override string ToString()
+        {
+            return "Visina= "+visina+", Sirina= "+sirina;
+        }
+    }
+}

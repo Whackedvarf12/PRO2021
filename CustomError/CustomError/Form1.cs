35515567e6ae47daad3a2e7fba8b2815662ce10a
Aly <aly.elkabani@gmail.com>
2021-11-12 13:21:02 +0100

    neg

---
+++ CustomError/CustomError/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CustomError
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int v = 0;
+                if (Int32.Parse(textBox1.Text)>-1)
+                {
+                    textBox2.Text = "" + Math.Sqrt(v);
+                }
+                else
+                {
+                    throw new NegativnoException();
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("format");
+            }
+            catch (NegativnoException f)
+            {
+                MessageBox.Show(f+"");
+            }
+        }
+    }
+}
---
+++ CustomError/CustomError/NegativnoException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomError
+{
+    class NegativnoException:ApplicationException
+    {
+        public NegativnoException() : base("neg") { }
+        public NegativnoException(String s) : base("neg"+s) { }
+    }
+}

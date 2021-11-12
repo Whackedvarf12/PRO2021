4ab687956581c187a8b448feb97613270f17d8d1
Aly <aly.elkabani@gmail.com>
2021-11-12 12:32:02 +0100

    del z 0

---
+++ Izjeme/Izjeme/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Izjeme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] i = new int[1];
+            try
+            {
+                Console.WriteLine(i[3]);
+            }
+            catch (Izjema e)
+            {
+
+            }
+        }
+    }
+}
---
+++ div0/div0/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace div0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                textBox3.Text = "" + Int32.Parse(textBox1.Text) / Int32.Parse(textBox2.Text);
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Napačni format");
+            }
+            catch (DivideByZeroException)
+            {
+                MessageBox.Show("Deljenje z 0");
+            }
+        }
+    }
+}
